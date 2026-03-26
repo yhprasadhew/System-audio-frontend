5799abae92d53bdacfda685f263c875b6f378a83
yhprasad <yhprasadhew@gmail.com>
2026-03-26 20:44:02 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './app.css'
 import Adminpage from './components/Adminpage'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Adminpage />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Adminpage />
+      </div>
+    </BrowserRouter>
   )
 }
 

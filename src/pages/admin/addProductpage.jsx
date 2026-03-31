fe49b74111367e79ea99c4d07cfba2ab7fb6d5c7
yhprasad <yhprasadhew@gmail.com>
2026-03-31 08:02:13 +0530

    ..

--- src/pages/admin/addProductpage.jsx
+++ src/pages/admin/addProductpage.jsx
@@ -17,27 +17,34 @@ export default function AdminaddItem() {
       <div className="w-[400px] bg-white shadow-md rounded-lg p-4 flex flex-col gap-3">
 
         <input 
+          onChange={(e) => setProductKey(e.target.value)}
           type="text" 
           placeholder="product key" 
           className="w-full border-2 border-gray-300 p-2 rounded focus:outline-none focus:border-green-500"
         />
 
 <input 
+          onChange={(e) => setName(e.target.value)}
           type="text" 
           placeholder="Name" 
           className="w-full border-2 border-gray-300 p-2 rounded focus:outline-none focus:border-green-500"
         />
         <input 
+          onChange={(e) => setPrice(e.target.value)}
           type="text" 
           placeholder="Price" 
           className="w-full border-2 border-gray-300 p-2 rounded focus:outline-none focus:border-green-500"
         />
-        <select>
+        <select 
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+        >
   <option key ="audio" value="audio">Audio</option>
   <option key ="video" value="video">Video</option>
   <option key ="accessories" value="accessories">Accessories</option>
 </select>
         <input 
+          onChange={(e) => setDimensions(e.target.value)} 
           type="text" 
           placeholder="Dimensions" 
           className="w-full border-2 border-gray-300 p-2 rounded focus:outline-none focus:border-green-500"
@@ -45,6 +52,7 @@ export default function AdminaddItem() {
 
 
         <input 
+          onChange={(e) => setDescription(e.target.value)}
           type="text" 
           placeholder="Description" 
           className="w-full border-2 border-gray-300 p-2 rounded focus:outline-none focus:border-green-500"
@@ -57,6 +65,9 @@ export default function AdminaddItem() {
         <button className="w-full bg-green-500 text-white font-bold py-2 rounded hover:bg-green-600 transition">
           Add Item
         </button>
+        <button className="w-full bg-gray-500 text-white font-bold py-2 rounded hover:bg-red-500 transition">
+          Cancel
+        </button> 
 
       </div>
 

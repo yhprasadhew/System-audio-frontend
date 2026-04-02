3a0d8f904dcfdc0b5b11a2ebaa849a7372ba3c4f
yhprasad <yhprasadhew@gmail.com>
2026-04-02 08:09:27 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Testing from './components/testing'
 import Login from './pages/login/loginPage'
 import { Toaster } from 'react-hot-toast'
+import Register from './pages/register/register'
 
 function App() {
   return (

2ae856b767effa2ac3b593ae965549bc6934bf65
yhprasad <yhprasadhew@gmail.com>
2026-04-03 11:57:05 +0530

    fix case-sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import './app.css'
 import Adminpage from './pages/admin/Adminpage'
-import Homepage from './pages/home/homepage'
+import Homepage from './pages/home/Homepage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Testing from './components/testing'
-import Login from './pages/login/loginPage'
+import Login from './pages/login/LoginPage'
 import { Toaster } from 'react-hot-toast'
-import Register from './pages/register/register'
+import Register from './pages/register/Register'
 
 function App() {
   return (
     <>
-      {/* ✅ Toast at TOP CENTER */}
       <Toaster 
         position="top-center"
         reverseOrder={false}
-        containerStyle={{
-          top: "20px",
-          zIndex: 99999
-        }}
+        containerStyle={{ top: "20px", zIndex: 99999 }}
         toastOptions={{
-          style: {
-            fontSize: "16px",
-            padding: "12px 20px"
-          }
+          style: { fontSize: "16px", padding: "12px 20px" }
         }}
       />
 
@@ -31,9 +24,9 @@ function App() {
           <Route path="/login" element={<Login />} /> 
           <Route path="/testing" element={<Testing />} />
           <Route path="/admin/*" element={<Adminpage />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/*" element={<Homepage />} />
           <Route path="*" element={<h1>404 Not Found</h1>} />
-          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>

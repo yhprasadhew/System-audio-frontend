98e39160b58e7f5c14cfd2481f49a77c9320fa1b
yhprasad <yhprasadhew@gmail.com>
2026-04-03 08:16:09 +0530

    ss

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -11,7 +11,7 @@ export default function Testing() {
         <div className="w-full h-screen">
             <h1 className="text-3xl font-bold text-center mt-10">Testing Page</h1> 
 
-            <input type ="file"/>
+            <input type="file" onChange={(e) => {setFile(e.target.files[0])}} className="block mx-auto mt-6" />
             <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">Upload</button>
         </div> 
     );

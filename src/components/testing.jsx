913ff59df5f5589515ffc3efa0be6f68c701087a
yhprasad <yhprasadhew@gmail.com>
2026-04-03 08:15:55 +0530

    ..

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 export default function Testing() {
-  
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };    
 
     return (    
         <div className="w-full h-screen">
-        
+            <h1 className="text-3xl font-bold text-center mt-10">Testing Page</h1> 
+
+            <input type ="file"/>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">Upload</button>
         </div> 
     );
 }
